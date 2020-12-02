6e8c0c0001efd1f8c46ab60f7e834bdb8836b8d0
ArjanKamphuis <arjan_kamphuis@hotmail.com>
2020-12-02 19:55:43 +0100

    Floor tile variation

--- Engine/Background.cpp
+++ Engine/Background.cpp
@@ -1,18 +1,31 @@
 #include "Background.h"
+
+#include <random>
 #include "SpriteEffect.h"
 
-Background::Background(const RectI& rect)
-	: mOrigin(rect.TopLeft())
-	, mGridWidth(div_int_ceil(rect.GetWidth(), mFloor.GetWidth()))
-	, mGridHeight(div_int_ceil(rect.GetHeight(), mFloor.GetHeight()))
+Background::Background(const RectI& bgRegion)
+	: mOrigin(bgRegion.TopLeft())
+	, mGridWidth(div_int_ceil(bgRegion.GetWidth(), mTileSize))
+	, mGridHeight(div_int_ceil(bgRegion.GetHeight(), mTileSize))
 {
+	for (int i = 0; i < mNumTiles; ++i)
+		mTileRects.emplace_back(Vec2I{ mTileSize, 0 } * i, mTileSize, mTileSize);
+
+	mTiles.reserve(static_cast<size_t>(mGridWidth * mGridHeight));
+	std::mt19937 rng(std::random_device{}());
+	std::uniform_int_distribution<int> dist(0, mNumTiles - 1);
+	for (int i = 0; i < mGridWidth * mGridHeight; ++i)
+		mTiles.push_back(dist(rng));
 }
 
 void Background::Draw(Graphics& gfx) const
 {
-	const int tileWidth = mFloor.GetWidth();
-	const int tileHeight = mFloor.GetHeight();
 	for (int y = 0; y < mGridHeight; ++y)
 		for (int x = 0; x < mGridWidth; ++x)
-			gfx.DrawSprite(x * tileWidth + mOrigin.X, y * tileHeight + mOrigin.Y, mFloor, SpriteEffect::Copy{});
+			gfx.DrawSprite(x * mTileSize + mOrigin.X, y * mTileSize + mOrigin.Y, mTileRects[GetTileAt(x, y)], mTileset, SpriteEffect::Copy{});
+}
+
+int Background::GetTileAt(int x, int y) const
+{
+	return mTiles[static_cast<size_t>(y * mGridWidth + x)];
 }
--- Engine/Background.h
+++ Engine/Background.h
@@ -1,16 +1,24 @@
 #pragma once
 
+#include <vector>
 #include "Graphics.h"
 
 class Background
 {
 public:
-	Background(const RectI& rect);
+	Background(const RectI& bgRegion);
 	void Draw(Graphics& gfx) const;
 
 private:
-	Surface mFloor = "Images/floor.bmp";
+	int GetTileAt(int x, int y) const;
+
+private:
+	Surface mTileset = "Images/floor5.bmp";
 	Vec2I mOrigin;
+	std::vector<RectI> mTileRects;
+	std::vector<int> mTiles;
+	int mNumTiles = 6;
+	int mTileSize = 32;
 	int mGridWidth;
 	int mGridHeight;
 };
